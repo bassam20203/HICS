2c208b6e5c318fd7c07984e856286eb080d47108
Bassam Adel <107225916+bassam20203@users.noreply.github.com>
2025-01-25 23:47:51 +0200

    Update Footer.jsx

--- src/Commponant/Footer.jsx
+++ src/Commponant/Footer.jsx
@@ -5,11 +5,11 @@ export default function Footer() {
 <div className='v'>
             <footer className="footer">
                 <div className="footer-content">
-                    <p>&copy; 2024 فريق مشروع تخرج (3) تحت اشراف دكتورة نهي - جميع الحقوق محفوظة.</p>
+                    <p>&copy; 2025 فريق مشروع تخرج (3) تحت اشراف دكتورة نهي - جميع الحقوق محفوظة.</p>
                     <div className="footer-links">
-                        <a href="#contact">اتصل بنا</a>
-                        <a href="#privacy">سياسة الخصوصية</a>
-                        <a href="#terms">الشروط والأحكام</a>
+                        <a href="https://h-i-c-s-m.blogspot.com/">الدخول للمنصة الرئيسية</a>
+                        
+                        <a href="https://h-i-c-s-m.blogspot.com/p/contact-us.html">اتصل بنا</a>
                     </div>
                 </div>
             </footer>
